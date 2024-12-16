4aa08e9b4fce98eb843e15c171a4428c41f29405
chaseez <chasezundel@gmail.com>
2024-12-16 16:45:01 -0700

    Got the search bar working

--- server.py
+++ server.py
@@ -7,6 +7,8 @@ from flask_cors import CORS
 
 import os
 
+from openai import OpenAI
+
 with open('secrets.env') as f:
     secrets = f.readlines()
 
@@ -16,7 +18,14 @@ for secret in secrets:
     name, password = name.strip(), password.strip()
     os.environ[name] = password
 
-# print(os.environ)
+
+# intialize OpenAI client
+
+openAiClient = OpenAI(
+    api_key = os.environ.get("openaiKey"),
+    organization = os.environ.get("orgId")
+)
+
 #replace with the right url, database, and collection
 myclient = pymongo.MongoClient("mongodb+srv://npseneca:xnPc5jSVrOoaur0t@cs452.gtgjg.mongodb.net/?retryWrites=true&w=majority&appName=cs452")
 mydb = myclient['testDB']
@@ -40,6 +49,12 @@ def Push():
 @app.route("/search", methods = ["Post", "GET"])
 def Search():
     try: 
+        with open('log.txt', 'a') as f:
+            print(request, file=f)
+        q = request.args.get('q')
+
+        
+
         query = {"zipcode": 20131}
         
         results = mycul.find(query, {"_id": 0, "detail_url": 1}).limit(5)
@@ -50,7 +65,22 @@ def Search():
     except Exception as e:
         return f"Error: {e}"
     
-    
+# OpenAI helper functions
+def getChatGptResponse(content):
+    instructions = commonSqlOnlyRequest = "Give me a MongoDB select statement that answers the question. Only respond with MongoDB syntax. If there is an error do not expalin it!"
+    stream = openAiClient.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[{"role": "user", "content": content}],
+        stream=True,
+    )
+
+    responseList = []
+    for chunk in stream:
+        if chunk.choices[0].delta.content is not None:
+            responseList.append(chunk.choices[0].delta.content)
+
+    result = "".join(responseList)
+    return result
 
 if __name__ == "__main__":
     app.run()
